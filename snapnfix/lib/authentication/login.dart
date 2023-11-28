d740e5f7c5be6b886b147f1d541131ac38f331b1
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2023-11-28 01:59:50 -0800

    Changed styling of login page

--- snapnfix/lib/authentication/login.dart
+++ snapnfix/lib/authentication/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,163 +9,219 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: const Color.fromRGBO(1, 32, 46, 1),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: const Text(
-                "SnapNFix",
-                style: TextStyle(
-                    color: Colors.white,
-                    decoration: TextDecoration.none,
-                    fontSize: 48),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                    color: Colors.white,
-                    decoration: TextDecoration.none,
-                    fontSize: 30),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white)),
-                    labelText: "Email",
-                    labelStyle: TextStyle(color: Colors.white),
-                    prefixIcon: Icon(
-                      Icons.email_outlined,
-                      color: Colors.white,
-                    )),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white)),
-                    labelText: "Password",
-                    labelStyle: TextStyle(color: Colors.white),
-                    prefixIcon: Icon(
-                      Icons.password_outlined,
-                      color: Colors.white,
-                    )),
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.all(10),
-                width: double.infinity,
-                height: 75,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: const ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(
-                      Color.fromRGBO(193, 104, 62, 1),
-                    ),
-                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        side: BorderSide(
-                            color: Color.fromRGBO(193, 104, 62, 1), width: 3))),
-                  ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Log in", style: TextStyle(fontSize: 20)),
-                      Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(
-                          Icons.arrow_circle_right_outlined,
-                          size: 25,
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-            const Padding(
-              padding: EdgeInsets.all(10),
-              child: Row(
+          width: double.infinity,
+          height: double.infinity,
+          color: const Color.fromRGBO(1, 32, 46, 1),
+          child: Center(
+            child: SingleChildScrollView(
+                child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: Divider(
-                      color: Colors.white,
-                      thickness: 2,
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      "SnapNFix",
+                      style: TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                          fontSize: 48),
                     ),
                   ),
-                  Padding(
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                          fontSize: 30),
+                    ),
+                  ),
+
+                  // Email Input
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: TextFormField(
+                      controller: _emailController,
+                      validator: (value) {
+                        if (value!.length < 10) {
+                          return "Enter Valid Email Address";
+                        }
+
+                        return null;
+                      },
+                      style: const TextStyle(color: Colors.black),
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromRGBO(241, 226, 226, 1),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        labelText: "Email",
+                        labelStyle:
+                            TextStyle(color: Color.fromRGBO(1, 32, 46, 1)),
+                        prefixIcon: Icon(
+                          Icons.email_outlined,
+                          color: Color.fromRGBO(1, 32, 46, 1),
+                        ),
+                        errorStyle: TextStyle(fontSize: 14,)
+                      ),
+                    ),
+                  ),
+
+                  // Password Input
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      style: const TextStyle(color: Colors.black),
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(241, 226, 226, 1),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          labelText: "Password",
+                          labelStyle:
+                              TextStyle(color: Color.fromRGBO(1, 32, 46, 1)),
+                          prefixIcon: Icon(
+                            Icons.password_outlined,
+                            color: Color.fromRGBO(1, 32, 46, 1),
+                          )),
+                    ),
+                  ),
+
+                  // Login button
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      width: double.infinity,
+                      height: 75,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            if (kDebugMode) {
+                              print(_emailController.text);
+                              print(_passwordController.text);
+                            }
+                          }
+                        },
+                        style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                            Color.fromRGBO(193, 104, 62, 1),
+                          ),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                  side: BorderSide(
+                                      color: Color.fromRGBO(193, 104, 62, 1),
+                                      width: 3))),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Log in", style: TextStyle(fontSize: 20)),
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Icon(
+                                Icons.arrow_circle_right_outlined,
+                                size: 25,
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+
+                  // break line
+                  const Padding(
                     padding: EdgeInsets.all(10),
-                    child: Text(
-                      "or",
-                      style: TextStyle(color: Colors.white, fontSize: 17.5),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.white,
+                            thickness: 2,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            "or",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 17.5),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.white,
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Expanded(
-                    child: Divider(
-                      color: Colors.white,
-                      thickness: 2,
-                    ),
-                  ),
+
+                  // signup button
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      width: double.infinity,
+                      height: 75,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(3, 58, 81, 1),
+                            ),
+                            shape: MaterialStatePropertyAll(
+                                RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                    side: BorderSide(
+                                        color: Color.fromRGBO(193, 104, 62, 1),
+                                        width: 3)))),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Sign Up", style: TextStyle(fontSize: 20)),
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Icon(
+                                Icons.arrow_circle_right_outlined,
+                                size: 25,
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+
+                  // Google Login
+                  IconButton(
+                      onPressed: () {},
+                      icon: Image.asset("assets/google-icon.png")),
                 ],
               ),
-            ),
-            Container(
-                padding: const EdgeInsets.all(10),
-                width: double.infinity,
-                height: 75,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(
-                        Color.fromRGBO(3, 58, 81, 1),
-                      ),
-                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          side: BorderSide(
-                              color: Color.fromRGBO(193, 104, 62, 1),
-                              width: 3)))),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Sign Up", style: TextStyle(fontSize: 20)),
-                      Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Icon(
-                          Icons.arrow_circle_right_outlined,
-                          size: 25,
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-          ],
-        ),
-      ),
+            )),
+          )),
     );
   }
 }
