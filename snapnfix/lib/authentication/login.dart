7e5d78c9ed73c42d1fd90ff5c1964d5938bfc29f
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2023-11-27 17:16:04 -0800

    Completed login page structure

--- snapnfix/lib/authentication/login.dart
+++ snapnfix/lib/authentication/login.dart
@@ -46,12 +46,16 @@ class _LoginViewState extends State<LoginView> {
                     fillColor: Colors.white,
                     enabledBorder: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                     focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.white)),
                     labelText: "Email",
                     labelStyle: TextStyle(color: Colors.white),
-                    prefixIcon: Icon(Icons.email_outlined, color: Colors.white,)),
+                    prefixIcon: Icon(
+                      Icons.email_outlined,
+                      color: Colors.white,
+                    )),
               ),
             ),
             Container(
@@ -59,18 +63,105 @@ class _LoginViewState extends State<LoginView> {
               child: TextFormField(
                 style: const TextStyle(color: Colors.white),
                 decoration: const InputDecoration(
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white)),
-                  labelText: "Password",
-                  labelStyle: TextStyle(color: Colors.white),
-                  prefixIcon: Icon(Icons.password_outlined, color: Colors.white,)
-                ),
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.white),
+                    prefixIcon: Icon(
+                      Icons.password_outlined,
+                      color: Colors.white,
+                    )),
               ),
             ),
+            Container(
+                padding: const EdgeInsets.all(10),
+                width: double.infinity,
+                height: 75,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                      Color.fromRGBO(193, 104, 62, 1),
+                    ),
+                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        side: BorderSide(
+                            color: Color.fromRGBO(193, 104, 62, 1), width: 3))),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Log in", style: TextStyle(fontSize: 20)),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.arrow_circle_right_outlined,
+                          size: 25,
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 2,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "or",
+                      style: TextStyle(color: Colors.white, fontSize: 17.5),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.all(10),
+                width: double.infinity,
+                height: 75,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                        Color.fromRGBO(3, 58, 81, 1),
+                      ),
+                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          side: BorderSide(
+                              color: Color.fromRGBO(193, 104, 62, 1),
+                              width: 3)))),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Sign Up", style: TextStyle(fontSize: 20)),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.arrow_circle_right_outlined,
+                          size: 25,
+                        ),
+                      )
+                    ],
+                  ),
+                )),
           ],
         ),
       ),
